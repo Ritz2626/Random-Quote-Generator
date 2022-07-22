6912f6078122768dc7e7611df72d0416bb15a943
dataritz <ritika.19b101173@abes.ac.in>
2022-07-23 03:04:25 +0530

    files included

---
+++ index.js
@@ -0,0 +1,16 @@
+const quoteTag=document.getElementById('quote-tags');
+const quoteAuthor=document.getElementById('quote-author');
+const quoteText=document.getElementById("quote-text");
+const generate=document.getElementById("generate");
+function randomQuote()
+{
+    fetch('http://api.quotable.io/random')
+    .then(response=>response.json())
+    .then(data=>{
+        quoteText.textContent=data.content;
+        quoteTag.textContent=data.tags;
+        quoteAuthor.textContent=`--${data.author}`;
+    });
+}
+randomQuote();
+generate.addEventListener('click',()=>{randomQuote()});
